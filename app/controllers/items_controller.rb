45b1aa4290401f3df9fad861052b2fe570d9a3be
Tucker Steil <tuckersteil@gmail.com>
2023-01-11 15:40:47 -0800

    commit tree 5d71b26965562a63b725f216d106b1fb59b8692d

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,34 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :bad_id
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+  def show 
+      item = Item.find(params[:id])
+      render json: item
+  end 
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create!(item_params)
+    render json: item, status: :created
+   end
+
+   private 
+
+   def item_params
+    params.permit(:name, :description, :price)
+   end 
+
+   def bad_id
+    render json: {error:"bad id"}, status: :not_found
+   end 
 end
